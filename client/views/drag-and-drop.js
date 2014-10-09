5f58eed4e7ac28449de6f94d5433279df2208798
mbe5038 <espejo.marius@gmail.com>
2014-10-09 08:36:37 -0400

    fixed drag drop problems

--- client/views/drag-and-drop.js
+++ client/views/drag-and-drop.js
@@ -3,16 +3,9 @@ Session.setDefault('mouseMoveElementId', 'Hello Dropzones!');
 Session.setDefault('mouseMoveX', 0);
 Session.setDefault('mouseMoveY', 0);
 
-Template.helloPage.helpers({
-  tables: function () {
-    return Tables.find();
-  }
-});
-
 Template.helloPage.greeting = function () {
   return "Welcome to drag-and-drop.";
 };
-
 
 Template.helloPage.rendered = function(){
   $(document).mousemove(function (e) {
@@ -34,11 +27,6 @@ Template.helloPage.rendered = function(){
     accept: ".dragDropBlock",
     drop: function(){
       $('#' + Session.get('movedElementId')).appendTo("#" + this.id);
-      var tblColor = "";
-      if (Session.get('movedElementId') == "redPanel") {tblColor="red"}
-      else if (Session.get('movedElementId') == "bluePanel") {tblColor="blue"}
-      else {tblColor="green"}
-      Tables.insert({name: tblColor });
       $('#' + Session.get('movedElementId')).css('top', '0');
       $('#' + Session.get('movedElementId')).css('left', '0');
       Session.clear('movedElementId');
