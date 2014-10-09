6449e150043b932e87796c1bc6668653de4b9a7f
AlexSaracino <alexsaracino@apples-MacBook-Pro.local>
2014-10-09 15:46:12 -0400

    Updated pulse html/js files

--- client/views/metrics/pulse/pulse.js
+++ client/views/metrics/pulse/pulse.js
@@ -1,5 +1,13 @@
 Template.pulse.helpers({
-	getHappy: function () {
-		return Pulse.find({status: 1});
-	}
+  getHappy: function () {
+    return Pulse.find({status: 1});
+  },
+
+  getNormal: function () {
+    return Pulse.find({status: 2});
+  },
+
+  getSad: function() {
+    return Pulse.find({status: 3});
+  }
 });
