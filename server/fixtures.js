1b1798f59b288dd20d37b8a7f4f9f44c059b7713
AlexSaracino <alexsaracino@apples-MacBook-Pro.local>
2014-10-09 16:02:04 -0400

    Updated fixtures, added if statement to Capcity data

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,48 +1,60 @@
 if (Pulse.find().count() === 0) {
 
-Pulse.insert({
-     userName: "Marius",
-     status: 3
-});
+    Pulse.insert({
+         userName: "Marius 'the beast' Espejo",
+         status: 3
+    });
 
-Pulse.insert({
-     userName: "KEvin",
-     status: 2
-});
+    Pulse.insert({
+         userName: "Ktan-asurus Rext",
+         status: 2
+    });
 
-Pulse.insert({
-     userName: "Davinder",
-     status: 3
-});
+    Pulse.insert({
+         userName: "Davinder",
+         status: 3
+    });
 
-Pulse.insert({
-     userName: "Alex",
-     status: 1
-});
+    Pulse.insert({
+         userName: "Alex 'Sass'acino",
+         status: 1
+    });
 
+    Pulse.insert({
+         userName: "Taco taco burrito",
+         status: 1
+    });
 
-Capacity.insert({
-    name:"Alex",
-    planned:"80",
-    actual:"80"
-})
+    Pulse.insert({
+         userName: "Russell Wilson",
+         status: 2
+    });
 
-Capacity.insert({
-    name:"Kevin",
-    planned:"80",
-    actual:"50"
-})
+}
 
-Capacity.insert({
-    name:"Davinder",
-    planned:"80",
-    actual:"70"
-})
+if (Capacity.find().count() === 0) {
 
-Capacity.insert({
-    name:"Marius",
-    planned:"80",
-    actual:"140"
-})
+    Capacity.insert({
+        name:"Alex",
+        planned:"80",
+        actual:"80"
+    });
 
+    Capacity.insert({
+        name:"Kevin",
+        planned:"80",
+        actual:"50"
+    });
+
+    Capacity.insert({
+        name:"Davinder",
+        planned:"80",
+        actual:"70"
+    });
+
+    Capacity.insert({
+        name:"Marius",
+        planned:"80",
+        actual:"140"
+    });
 }
