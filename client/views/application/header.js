a3862cfeb64c3859bfbdc510c382231b27dab0d7
mbe5038 <espejo.marius@gmail.com>
2014-10-10 04:02:22 -0400

    fixed nav link probs. I think

--- client/views/application/header.js
+++ client/views/application/header.js
@@ -2,23 +2,83 @@
  * Created by kevin on 10/9/14.
  */
 Template.header.events({
-    'click #viewHuddleLink' : function(e) {
+    'click #huddleBoard' : function(e) {
      console.log(e);
-        $("#createHuddleLink").removeClass("active");
-        $('#viewHuddleLink').addClass("active");
+        $("#canvas").removeClass("active");
+        $("#announcements").removeClass("active");
+        $("#pulse").removeClass("active");
+        $("#capacity").removeClass("active");
+        $("#calendar").removeClass("active");
+        $('#huddleBoard').addClass("active");
 
     }
 
 });
 
+
 Template.header.events({
-    'click #createHuddleLink' : function(e) {
+    'click #canvas' : function(e) {
         console.log(e);
-        $("#viewHuddleLink").removeClass("active");
-        $('#createHuddleLink').addClass("active");
+        $("#huddleBoard").removeClass("active");
+        $("#announcements").removeClass("active");
+        $("#pulse").removeClass("active");
+        $("#capacity").removeClass("active");
+        $("#calendar").removeClass("active");
+        $('#canvas').addClass("active");
 
     }
+});
 
+Template.header.events({
+    'click #announcements' : function(e) {
+        console.log(e);
+        $("#huddleBoard").removeClass("active");
+        $("#canvas").removeClass("active");
+        $("#pulse").removeClass("active");
+        $("#capacity").removeClass("active");
+        $("#calendar").removeClass("active");
+        $('#announcements').addClass("active");
+
+    }
+});
+
+Template.header.events({
+    'click #pulse' : function(e) {
+        console.log(e);
+        $("#huddleBoard").removeClass("active");
+        $("#canvas").removeClass("active");
+        $("#announcements").removeClass("active");
+        $("#capacity").removeClass("active");
+        $("#calendar").removeClass("active");
+        $('#pulse').addClass("active");
+
+    }
+});
+
+Template.header.events({
+    'click #capacity' : function(e) {
+        console.log(e);
+        $("#huddleBoard").removeClass("active");
+        $("#canvas").removeClass("active");
+        $("#pulse").removeClass("active");
+        $("#announcements").removeClass("active");
+        $("#calendar").removeClass("active");
+        $('#capacity').addClass("active");
+
+    }
+});
+
+Template.header.events({
+    'click #calendar' : function(e) {
+        console.log(e);
+        $("#huddleBoard").removeClass("active");
+        $("#canvas").removeClass("active");
+        $("#pulse").removeClass("active");
+        $("#capacity").removeClass("active");
+        $("#announcements").removeClass("active");
+        $('#calendar').addClass("active");
+
+    }
 });
 
 Template.header.helpers({
